f2940c7bc7b7155f152f71a2c8b2a92b36bbb491
Slawomir Kania <slawomir.kania1@gmail.com>
2023-05-02 18:28:19 +0200

    http4s middleware

--- src/test/scala/Http4sSuite.scala
+++ src/test/scala/Http4sSuite.scala
@@ -14,21 +14,39 @@ class Http4sSuite extends CatsEffectSuite {
   import Http4sSuite._
 
   test("Response decoding") {
-    val result: IO[Response[IO]] = routes.run(Request[IO](Method.GET, uri"/hello/f/b"))
+    val result: IO[Response[IO]] = routes.orNotFound.run(validRequest)
 
     assertIO(result.flatMap(_.as[FooBar]), FooBar("f", "b"))
   }
 
   test("404") {
-    val result: IO[Response[IO]] = routes.run(Request[IO](Method.GET, uri"/invalid"))
+    val result: IO[Response[IO]] = routes.orNotFound.run(invalidRequest)
 
     assertIO(result.map(_.status), NotFound)
   }
 
   test("client") {
-    val client: Client[IO] = Client.fromHttpApp(routes)
+    val client: Client[IO] = Client.fromHttpApp(routes.orNotFound)
 
-    assertIO(client.expect[FooBar](Request[IO](Method.GET, uri"/hello/f/b")), FooBar("f", "b"))
+    assertIO(client.expect[FooBar](validRequest), FooBar("f", "b"))
+  }
+
+  test("middleware - valid request") {
+    val result = Middleware(routes).orNotFound.run(validRequest)
+
+    assertIO(
+      result.map(_.headers.headers.map(_.value)),
+      List("text/plain; charset=UTF-8", "24", "someValue")
+    )
+  }
+
+  test("middleware - invalid request") {
+    val result = Middleware(routes).orNotFound.run(invalidRequest)
+
+    assertIO(
+      result.map(_.headers.headers.map(_.value)),
+      List("text/plain; charset=UTF-8", "9")
+    )
   }
 }
 
@@ -48,5 +66,15 @@ object Http4sSuite {
     .of[IO] { case GET -> Root / "hello" / foo / bar =>
       Ok(s"""{"foo": "$foo", "bar": "$bar"}""")
     }
-    .orNotFound
+
+  val validRequest   = Request[IO](Method.GET, uri"/hello/f/b")
+  val invalidRequest = Request[IO](Method.GET, uri"/invalid")
+
+  object Middleware {
+    def addHeader(response: Response[IO])(headers: Header.ToRaw): Response[IO] =
+      response.putHeaders(headers)
+
+    def apply(routes: HttpRoutes[IO]): HttpRoutes[IO] =
+      routes.map(addHeader(_)("someKey" -> "someValue"))
+  }
 }
