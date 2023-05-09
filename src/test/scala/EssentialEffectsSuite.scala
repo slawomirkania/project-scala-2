5422ed43ac53656de52c88cbd012c8756e70dd22
Slawomir Kania <slawomir.kania1@gmail.com>
2023-05-09 09:19:22 +0200

    MyIO

---
+++ src/test/scala/EssentialEffectsSuite.scala
@@ -0,0 +1,51 @@
+package com.example
+
+import munit.CatsEffectSuite
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.FiniteDuration
+
+class EssentialEffectsSuite extends CatsEffectSuite with EssentialEffectsSuiteContext {
+  test("unsafeRun") {
+    val helloWorld = for {
+      _ <- MyIO.putStr("hello")
+      _ <- MyIO.putStr("world")
+    } yield ()
+
+    helloWorld.unsafeRun()
+  }
+
+  test("Timing") {
+    val clock: MyIO[Long] = MyIO(() => System.currentTimeMillis())
+
+    def time[A](action: MyIO[A]): MyIO[(FiniteDuration, A)] =
+      for {
+        start  <- clock
+        result <- action
+        end    <- clock
+      } yield (FiniteDuration(end - start, TimeUnit.MILLISECONDS), result)
+
+    val helloWorld = for {
+      _ <- MyIO.putStr("hello")
+      _ <- MyIO(() => Thread.sleep(100L))
+      _ <- MyIO.putStr("world")
+    } yield ()
+
+    val timedHello = time(helloWorld)
+
+    timedHello.unsafeRun() match {
+      case (duration, _) => println(s"hello took $duration")
+    }
+  }
+}
+
+trait EssentialEffectsSuiteContext {
+  case class MyIO[A](unsafeRun: () => A) {
+    def map[B](f: A => B): MyIO[B]           = MyIO(() => f(unsafeRun()))
+    def flatMap[B](f: A => MyIO[B]): MyIO[B] = MyIO(() => f(unsafeRun()).unsafeRun())
+  }
+
+  object MyIO {
+    def putStr(s: => String): MyIO[Unit] = MyIO(() => println(s))
+  }
+}
