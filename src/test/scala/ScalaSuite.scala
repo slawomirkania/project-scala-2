6c1b1abff68db39537de5670b48f8345538ddf5b
Slawomir Kania <slawomir.kania1@gmail.com>
2023-09-21 09:30:28 +0200

    type class practice

--- src/test/scala/ScalaSuite.scala
+++ src/test/scala/ScalaSuite.scala
@@ -10,7 +10,7 @@ import munit.CatsEffectSuite
 import scala.math.BigDecimal.RoundingMode
 
 class ScalaSuite extends CatsEffectSuite {
-  test("Type class") {
+  test("Type class v1") {
     final case class Square(a: Double)
     final case class Rectangle(a: Double, b: Double)
     final case class Circle(r: Double)
@@ -27,13 +27,48 @@ class ScalaSuite extends CatsEffectSuite {
       implicit val circleAreaCalculator: Area[Circle]       = (s: Circle) => halfUp(s.r * Math.PI)
     }
 
-    implicit class AreaOps[S: Area](s: S) {
+    implicit class AreaOps[S: Area](s: S) { // extension
       def area: Double = implicitly[Area[S]].calculate(s)
     }
 
     assertEquals(Square(5.2).area, 27.04)
     assertEquals(Rectangle(5.2, 2.54).area, 13.21)
     assertEquals(Circle(2.3).area, 7.23)
+  }
+
+  test("Type class v2") {
+    final case class Square(a: Double)
+    final case class Rectangle(a: Double, b: Double)
+    final case class Circle(r: Double)
+
+    trait Area[S] {
+      def calculate(s: S): Double
+    }
+
+    def halfUp(d: Double) = BigDecimal.apply(d).setScale(2, RoundingMode.HALF_UP).doubleValue
+
+    implicit object SquareArea extends Area[Square] {
+      override def calculate(s: Square): Double = halfUp(Math.pow(s.a, 2.0))
+    }
+
+    implicit object RectangleArea extends Area[Rectangle] {
+      override def calculate(s: Rectangle): Double = halfUp(s.a * s.b)
+    }
+
+    implicit object CircleArea extends Area[Circle] {
+      override def calculate(s: Circle): Double = halfUp(s.r * Math.PI)
+    }
+
+    def area[T](shape: T)(implicit area: Area[T]): Double = area.calculate(shape)
+    def area2[T: Area](shape: T): Double                  = implicitly[Area[T]].calculate(shape)
+
+    assertEquals(area(Square(5.2)), 27.04)
+    assertEquals(area(Rectangle(5.2, 2.54)), 13.21)
+    assertEquals(area(Circle(2.3)), 7.23)
+
+    assertEquals(area2(Square(5.2)), 27.04)
+    assertEquals(area2(Rectangle(5.2, 2.54)), 13.21)
+    assertEquals(area2(Circle(2.3)), 7.23)
   }
 
   test("Variance") {
